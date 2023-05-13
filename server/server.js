5832710ff0ba21ff67b132c20a1a14f719757f76
divyapoc <divya.lk23@gmail.com>
2023-05-13 14:09:00 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -7,10 +7,11 @@ import v1 from "./v1/index.js";
 dotenv.config();
 
 const app = express();
-const corsOptions = {
-  origin: "https://hospitalmanagementwebsite.onrender.com", // frontend URI (ReactJS)
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://hospitalmanagementwebsite.onrender.com", // frontend URI (ReactJS)
+// };
+// app.use(cors(corsOptions));
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/v1", v1);
