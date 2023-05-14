884a65407a31659c2dafc8463900836381b7142a
divyapoc <divya.lk23@gmail.com>
2023-05-14 21:10:05 +0530

    cors erorr fix

--- server/server.js
+++ server/server.js
@@ -11,6 +11,19 @@ const corsOptions = {
   origin: "https://hospitalmanagementwebsite.onrender.com", // frontend URI (ReactJS)
 };
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000','https://hospitalmanagementwebsite.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 // app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
